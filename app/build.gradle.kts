b634732782ab879fb3f24a81f41eaab5d052f7f0
Hemanth <saihemanth225@gmail.com>
2026-09-21 11:30:09 +0530

    Add Material Icons Extended dependency
    Added the `material-icons-extended` library to the project's dependencies in `libs.versions.toml` and `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.compose.material3:material3")
+    implementation(libs.androidx.material.icons.extended)
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
